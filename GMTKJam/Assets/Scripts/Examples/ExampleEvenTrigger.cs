5f87b09d5a696f941459cd65bed22c9d14d5ceb3
Adam <imposter110@gmail.com>
2017-07-15 00:16:46 -0700

    Added basic message system plus examples

---
+++ GMTKJam/Assets/Scripts/Examples/ExampleEvenTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleEvenTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1"))
+        {
+            EventManager.TriggerEvent("DebugA");
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            EventManager.TriggerEvent("DebugB");
+        }
+    }
+}
---
+++ GMTKJam/Assets/Scripts/Examples/ExampleEventMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ExampleEventMove : MonoBehaviour {
+
+    void OnEnable()
+    {
+        EventManager.StartListening("DebugA", DebugA);
+        EventManager.StartListening("DebugB", DebugB);
+    }
+
+    void OnDisable()
+    {
+        EventManager.StopListening("DebugA", DebugA);
+        EventManager.StopListening("DebugB", DebugB);
+    }
+
+
+    void DebugA()
+    {
+        Debug.Log("you have debugged A");
+    }
+
+    void DebugB()
+    {
+        Debug.Log("you have debugged B");
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
